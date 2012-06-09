cf32ee3c3ea66207ded2300f217bb2ed49b1af57
mkl <mkl@ableton.com>
2012-06-09 14:30:57 +0200

    Moved test running into vim

--- plugin/lldb.py
+++ plugin/lldb.py
@@ -1,17 +1,38 @@
 import lldb
 import os
 import time
-
-def log(message):
-  with open("lldb_log.txt", "a") as f:
-    f.write(str(time.time()) + " - ")
-    f.write(str(message) + "\n")
+import tempfile
+import unittest
 
 def run_target(target_filename):
   debugger = lldb.SBDebugger.Create()
   debugger.SetAsync(False)
   target = debugger.CreateTarget(target_filename)
   if not target:
-    log("Failed to get a target")
+    raise "Failed to get a target"
 
-  target.LaunchSimple (None, None, os.getcwd())
+  process = target.LaunchSimple (None, None, os.getcwd())
+
+class TestLLDBPlugin(unittest.TestCase):
+  def test(self):
+    c_source = """int main()
+    {
+      return 0;
+    }
+    """
+
+    temp_dir = tempfile.mkdtemp()
+    source_file = tempfile.NamedTemporaryFile(dir = temp_dir)
+
+    source_file.write(c_source)
+    source_file.flush()
+
+    out_name = os.path.join(temp_dir, "binary")
+    os.system("clang -g -x c %s -o %s" % (source_file.name, out_name))
+
+    run_target(out_name)
+
+def run_lldb_tests():
+  suite = unittest.TestLoader().loadTestsFromTestCase(TestLLDBPlugin)
+  with open("test_log.txt", "a") as f:
+    unittest.TextTestRunner(stream=f, verbosity=2).run(suite)
